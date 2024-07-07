6466545aeebed4319ef7df71d79a72ffe4aca1ad
Moustafa345 <moustafamouhamed5@gmail.com>
2024-07-08 02:00:27 +0300

    Final Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <section>
       <Hero />
-      <h2 className=" text-center mt-10 text-3xl font-bold">
+      <h2 className=" text-center mt-10 text-3xl font-bold px-2">
         Choose your web hosting plan
       </h2>
       <div className="container m-auto flex justify-center items-center my-7 flex-wrap md:gap-7">
